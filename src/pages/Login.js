0ea76dc87b673c126c068527ad6cb5118ac747de
heitorgsantos <heitorg33@gmail.com>
2022-10-21 15:03:17 -0300

    Configurando Janela de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import validaEmail from "../validations/validationData";
-import validaSenha from "../validations/validationData";
+import { validaEmail } from "../validations/validationData";
+import { validaSenha } from "../validations/validationData";
 
 import { useNavigate } from "react-router-dom";
 
@@ -8,43 +8,39 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  // const handleSubmit = () => {
-  //   if (validaEmail(email) && validaSenha(password) === true) {
-  //     return true;
-  //   }
-  // };
+  const handleSubmit = () => {
+    validaEmail(email) && validaSenha(password) === true
+      ? navigate("/home")
+      : console.log("Falha na autorização");
+  };
 
   return (
-    <div class='container' >
-      <form >
-        <div class="mb-3">
-          <label class="form-label" for="email">
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="email">
             Email
           </label>
           <input
             id="email"
             type="email"
             value={email}
-            class="form-control"
+            className="form-control"
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div class="mb-3">
-          <label class="form-label" for="password">
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">
             Senha
           </label>
           <input
-            class="form-control"
+            className="form-control"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button
-          onClick={() => (validaEmail(email) && validaSenha(password) === true ? navigate("/home") : console.log("Falha na autorização"))}
-          type="submit"
-          class="btn btn-primary"
-        >
+        <button type="submit" className="btn btn-primary">
           Enviar
         </button>
       </form>
