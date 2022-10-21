a0045372f883f4634b6b6b8001e7932c875e3ee3
heitorgsantos <heitorg33@gmail.com>
2022-10-21 15:56:14 -0300

    Corrigindo nome das classes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { validaCNPJ } from "../validations/validateCnpj";
 import { Navigate, useNavigate } from "react-router-dom";
 import { InputData } from "./components/DadosCnpj";
 import HeaderMain from "../components/HeaderMain";
+import styled from "styled-components";
 function Home() {
   // 47960950000121
   const [numCnpj, setNumCnpj] = useState("");
@@ -35,7 +36,7 @@ function Home() {
   return (
     <div>
       <HeaderMain />
-      <div className=" position-absolute top-50 start-50 translate-middle w-80 h-50 shadow p-3 mb-5 bg-body rounded">
+      <div className=" ">
         <input
           class="form-control mb-5 mt-3"
           type="text"
@@ -47,28 +48,34 @@ function Home() {
         />
         <div>
           <button
-            class="btn btn-primary"
+            className="btn btn-primary"
             disabled={validaCNPJ(numCnpj) === false ? btnDBuscar : false}
             onClick={() => handleFetchApi()}
           >
             Buscar
           </button>
           <button
-            class="btn btn-primary mx-3"
+            className="btn btn-primary mx-3"
             disabled={btnDResultados}
             onClick={() => handlePage()}
           >
             Ver Resultados
           </button>
           {/* <Navigate to="/resultados" state={resultFetch}/> */}
-          <button class="btn btn-primary" onClick={handleClick}>
+          <button className="btn btn-primary" onClick={handleClick}>
             Nova Busca
           </button>
         </div>
-        <h3 class="d-flex justify-content-center mt-5">Buscador de Dados</h3>
+        <h3 className="d-flex justify-content-center mt-5">Buscador de Dados</h3>
+
       </div>
     </div>
   );
 }
+
+
+const CnpjInvalido = styled.p `
+  display  = ``
+`
 
 export default Home;
